f49e377af2c68f4ef717ddce654dc96817ba7915
MicheleRai <rainonemichele1999@gmail.com>
2022-12-14 16:38:14 +0100

    fix and improve snack 1

--- ## Snack 1/index.php
+++ ## Snack 1/index.php
@@ -4,48 +4,48 @@
 Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60
 */ 
-$partite_array = array (
-    'partita' => array (
-        'casa' => array (
+$partite_array = [
+    ['partita' => [
+        'casa' => [
             'nome' => 'Milano',
             'score' => 66,
-        ),
-        'ospite' => array (
+        ],
+        'ospite' => [
             'nome' => 'Bari',
             'score' => 100,
-        ),  
-    ),
-    'partita' => array (
-        'casa' => array (
+        ],  
+    ]],
+    ['partita' => [
+        'casa' => [
             'nome' => 'Roma',
             'score' => 15,
-        ),
-        'ospite' => array(
+        ],
+        'ospite' => [
             'nome' => 'Trieste',
             'score' => 47,
-        ), 
-    ),
-    'partita' => array (
-        'casa' => array (
+        ], 
+    ]],
+    ['partita' => [
+        'casa' => [
             'nome' => 'Bologna',
             'score' => 110,
-        ),
-        'ospite' => array(
+        ],
+        'ospite' => [
             'nome' => 'Firenze',
             'score' => 85,
-        ),
-    ),
-    'partita' => array (
-        'casa' => array (
+    ]],
+    ],
+    ['partita' => [
+        'casa' => [
             'nome' => 'Genova',
             'score' => 100,
-        ),
-        'ospite' => array(
+        ],
+        'ospite' => [
             'nome' => 'Torino',
             'score' => 71,
-        ),  
-    ),
-);
+    ]   ,  
+    ]]
+];
 ?>
 <?= '<pre>' . print_r($partite_array, true) . '</pre>' ?>
 <!DOCTYPE html>
@@ -60,8 +60,8 @@ $partite_array = array (
     <h1>PARTITE GIOCATE OGGI</h1>
     <ul>
     <?php
-    for ($i=0; $i < count($partite_array); ; $i++) { 
-        echo"<li>$partite_array[$i]["partita"]["casa"]["nome"] - $partite_array[$i]["partita"]["ospite"]["nome"] | $partite_array[$i]["partita"]["casa"]["score"] - $partite_array[$i]["partita"]["ospite"]["score"] </li>";
+    for ($i=0; $i < count($partite_array); $i++) { 
+        echo "<li>" . $partite_array[$i]['partita']['casa']['nome'] . " - " . $partite_array[$i]['partita']['ospite']['nome'] . " | " . $partite_array[$i]['partita']['casa']['score'] . " - " . $partite_array[$i]['partita']['ospite']['score'] . "</li>";
     };
     ?>
     </ul>
